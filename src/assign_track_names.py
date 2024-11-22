357138551093f1a1dc41be5e3f07eabb8db127c2
Theo Bernier <theo.bernier@climatepartner.com>
2024-11-22 14:51:44 +0100

    refactor: rename files

---
+++ src/assign_track_names.py
@@ -0,0 +1,42 @@
+import os
+import pandas as pd
+
+from mutagen.mp3 import MP3
+from mutagen.easyid3 import EasyID3
+
+def getPlaylist(filePath):
+    columns = ['Nom du titre', "Nom(s) de l'artiste de l'album", "Nom de l'album", "Nom(s) de l'artiste"]
+    playlist_df = pd.read_csv(filePath)
+    return playlist_df[columns]
+
+def reNumberFileNames(directoryPath):
+    os.chdir(directoryPath)
+    for i in range(32,33):
+        number = f'{i}'
+        if i < 10:
+            number = f'0{i}'
+        currentFileName = f'Dowload--{number}.mp3'
+        newFileName = f'Doownload-{number}.mp3'
+
+        os.rename(currentFileName,newFileName)
+
+
+def applyTrackInfo(filePath, trackInfo):
+    mp3file = MP3(filePath, ID3=EasyID3)
+
+    mp3file['title'] = trackInfo['Nom du titre']
+    mp3file['album'] = trackInfo["Nom de l'album"]
+    mp3file['albumartist'] = trackInfo["Nom(s) de l'artiste de l'album"]
+    mp3file['artist'] = trackInfo["Nom(s) de l'artiste"]
+
+    mp3file.save()
+
+playlistDf = getPlaylist("/Users/theobernier/Music/Téléchargements/download_25_10_24/track_infos.csv")
+
+for index, trackInfo in playlistDf.iterrows():
+    number = f'{index+1}'
+    if index < 9:
+        number = f'0{index+1}'
+    filePath = "/Users/theobernier/Music/Téléchargements/download_25_10_24/Doownload-%s.mp3" %(number)
+    print(filePath)
+    applyTrackInfo(filePath, trackInfo)
